ce9fcfa7b331e9801ad307fc32a90c7dfa1f52ea
Ruairi O Brien <ruairitobrien@gmail.com>
2019-12-30 14:38:29 +0000

    Added ionic

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,37 @@
-import React from 'react'
-import { Formik, Form, Field, ErrorMessage, FormikErrors } from 'formik'
-import './App.css'
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
 
-const encode = (data: any) => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
 
-interface FormValues {
-  email: string
-}
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
-const App: React.FC = () => {
-  const initialValues: FormValues = { email: '' }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div data-netlify-identity-menu></div>
-      </header>
-      <main>
-        <Formik
-          initialValues={initialValues}
-          validate={(values: FormValues) => {
-            const errors: FormikErrors<FormValues> = { email: '' }
-            if (!values.email) {
-              errors.email = 'Required'
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address'
-            }
-            return errors
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              fetch('/', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: encode({ 'evg-form': 'contact', values })
-              })
-                .then(() => alert('Success!'))
-                .catch(error => alert(error))
-              setSubmitting(false)
-            }, 400)
-          }}
-        >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field type="email" name="email" />
-              <ErrorMessage name="email" component="div" />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </main>
-    </div>
-  )
-}
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
-export default App
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route path="/home" component={Home} exact={true} />
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
---
+++ src/AppForm.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage, FormikErrors } from 'formik'
+import './App.css'
+
+const encode = (data: any) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+interface FormValues {
+  email: string
+}
+
+const App: React.FC = () => {
+  const initialValues: FormValues = { email: '' }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div data-netlify-identity-menu></div>
+      </header>
+      <main>
+        <Formik
+          initialValues={initialValues}
+          validate={(values: FormValues) => {
+            const errors: FormikErrors<FormValues> = { email: '' }
+            if (!values.email) {
+              errors.email = 'Required'
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Invalid email address'
+            }
+            return errors
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              fetch('/', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: encode({ 'evg-form': 'contact', values })
+              })
+                .then(() => alert('Success!'))
+                .catch(error => alert(error))
+              setSubmitting(false)
+            }, 400)
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </main>
+    </div>
+  )
+}
+
+export default App
